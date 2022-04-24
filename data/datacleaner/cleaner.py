678e79944d54912f0d8ee32c5438228d75465ca8
Ethan Lee <33e.lee@gmail.com>
2022-04-24 16:24:35 -0700

    Update function so it wont stop program if error encountered

--- data/datacleaner/cleaner.py
+++ data/datacleaner/cleaner.py
@@ -8,8 +8,9 @@ def add_ticker_column_and_populate(dataframe: DataFrame, ticker_name: str) -> Da
     return dataframe
 
 def remove_columns(dataframe: DataFrame, column_name, in_place=True) -> DataFrame:
-    if column_name in dataframe.columns:
+    try:
         return dataframe.drop(column_name, axis=1, inplace=in_place)
-
-    raise Exception("Trying to remove a column that doesn't exist")
+    except KeyError:
+        print("column: {} doesn't exist".format(column_name))
+        pass
 
